ef3eaaa4579d1e4badff4266d160fa08bc3bb5e6
Maksymilian Zadka <maksymilian.j.zadka@gmail.com>
2023-02-10 12:35:07 +0100

    styling

--- src/components/TodosGrid.tsx
+++ src/components/TodosGrid.tsx
@@ -14,10 +14,31 @@ export default function TodosGrid() {
   }
 
   return (
-    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-      {data.map(({ id, text, state }) => (
-        <TodoCard key={id} text={text} state={state as TODO_STATES_TYPES} />
-      ))}
+    <div className="flex flex-col gap-4">
+      <p className="text-xl font-semibold">Started</p>
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+        {data
+          .filter(({ state }) => state === "STARTED")
+          .map(({ id, text, state }) => (
+            <TodoCard key={id} text={text} state={state as TODO_STATES_TYPES} />
+          ))}
+      </div>
+      <p className="text-xl font-semibold">Completed</p>
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+        {data
+          .filter(({ state }) => state === "COMPLETED")
+          .map(({ id, text, state }) => (
+            <TodoCard key={id} text={text} state={state as TODO_STATES_TYPES} />
+          ))}
+      </div>
+      <p className="text-xl font-semibold">Abandoned</p>
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+        {data
+          .filter(({ state }) => state === "ABANDONED")
+          .map(({ id, text, state }) => (
+            <TodoCard key={id} text={text} state={state as TODO_STATES_TYPES} />
+          ))}
+      </div>
     </div>
   );
 }
